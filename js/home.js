7a6e0d46586a00b1324aa7e314c83428c3f1fd54
Tharindu D Munasinghe <85168846+tharindu-dm@users.noreply.github.com>
2023-04-13 20:03:09 +0530

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,7 @@
+function customLocation(){
+    var form = document.getElementById("formRentSearch").innerHTML;
+}
+
+//check pick up date not old.
+// chekc drop off date is on or after pick up date
+//check times are both future
