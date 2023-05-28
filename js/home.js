76289e729fecb0ace14de05a7bad2bf1aeb90e5a
Tharindu D Munasinghe <85168846+tharindu-dm@users.noreply.github.com>
2023-05-28 20:02:03 +0530

    Delete js directory

--- js/home.js
+++
@@ -1,7 +0,0 @@
-function customLocation(){
-    var form = document.getElementById("formRentSearch").innerHTML;
-}
-
-//check pick up date not old.
-// chekc drop off date is on or after pick up date
-//check times are both future
